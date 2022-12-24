65093ff31b6bc45a92af5d5c8594b320da0a918d
Pse1234 <pepegarcia_9@hotmail.com>
2022-12-24 14:52:47 +0000

    new folder for the basic app :)

---
+++ app/install2.sh
@@ -0,0 +1,11 @@
+#conda prefix
+ENV=venv
+
+# Load conda specific commands.
+#source  ./miniconda3/envs/
+ 
+# Activate the environment.conda activate $ENV
+
+source activate /home/group6/miniconda3/envs/venv
+
+# Print conda prefix	echo PREFIX:$CONDA_PREFIX

b23a4ed6cc977d6595f2388b6c99a81520c26c6d
JAGS <78938133+Pse1234@users.noreply.github.com>
2022-12-25 18:22:00 +0100

    random done 🥰👌🏻

--- webapp/linux_app/views.py
+++ webapp/linux_app/views.py
@@ -4,6 +4,8 @@ from django.views.generic import View
 from django.shortcuts import render
 import pandas as pd
 import json
+import random
+import os
 
 
 class HomeView(View):
@@ -18,15 +20,26 @@ def home(request):
         get_cherche  = request.POST.get("cherche_moi_ca_stp")
         df = reco.running('./model/data.csv',f"{get_cherche}")
         df = df.reset_index()
+
+        df_random = pd.read_csv('./model/data.csv', sep=";")
+        random_number = random.randrange(df_random.shape[0])
+        df_random = df_random.loc[[random_number], ['title','lead_text','date_start','date_end','address_street','price_type','url']]
+
         json_records = df.to_json(orient ='records')
         arr = []
         arr = json.loads(json_records)
+
+        json_records_random = df_random.to_json(orient ='records')
+        arr_random = []
+        arr_random = json.loads(json_records_random)
+
         contextt = {
-            'd': arr
+            'd': arr,
+            'r': arr_random,
             }
 
         return  render(request, 'result.html', contextt)
-
+        
  # Create your views here.
 def index(request):
     return render(request,'index.html')

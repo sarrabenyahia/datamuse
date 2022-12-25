d83159b452646813fc753f94cae7df6034b2d36d
Pse1234 <pepegarcia_9@hotmail.com>
2022-12-25 20:01:46 +0000

    docker app complete :rocket:

--- app_docker/launch.sh
+++ app_docker/launch.sh
@@ -4,8 +4,24 @@ bash data_getter.sh ./target/data.csv
 
 echo "Recommendation system computing"
 sudo docker run --mount type=bind,source="$(pwd)"/target,target=/app datamuse $1 $2 
+echo "     "
+sleep 1s
 echo "Hacking Nasa"
+sleep 1s
+echo "Loading ..."
+sleep 0.2s
+echo "20%"
+sleep 0.2s
+echo "40%"
+sleep 0.2
+echo "60%"
+sleep 0.2
+echo "80%"
+sleep 0.2
+echo "99%"
 sleep 2s
+echo "Nasa hacked"
+sleep 1s
 echo "..."
 sleep 2s 
 echo "Just kidding, end of recommendation system"

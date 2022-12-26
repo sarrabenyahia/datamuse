eed5d5da114bc517bd345740e14c9c1e152ab348
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2022-12-26 12:42:16 +0100

    comments clean

--- app_docker/run.py
+++ app_docker/run.py
@@ -2,20 +2,9 @@ import os
 import sys
 
 
-
-#Step 2 : On importe avec le data_getter
-
-
-#Step 3 : On effectue la recherche 
 search = sys.argv[1]
 command_recherche = f'python reco.py ./data.csv {search}'
 os.system(command_recherche)
-#subprocess.call([command_recherche, search])
 
 
 
-
-#Step 1 : On effectue la recherche 
-#search = sys.argv[1]
-#command_recherche = f'python reco.py ./datamuse/app_docker/data.csv {search}'
-#os.system(command_recherche)
